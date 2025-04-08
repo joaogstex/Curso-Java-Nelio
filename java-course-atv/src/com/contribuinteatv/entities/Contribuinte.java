4207a21a88c9b57973624084fede14f3ad450fb1
joaogstex <joaogstex@gmail.com>
2025-04-08 17:25:05 -0300

    initial reservation - more to add

---
+++ java-course-atv/src/com/contribuinteatv/entities/Contribuinte.java
@@ -0,0 +1,5 @@
+package com.contribuinteatv.entities;
+
+public abstract class Contribuinte {
+    
+}
---
+++ java-course-atv/src/reservationatv/model/entities/Reservation.java
@@ -0,0 +1,57 @@
+package reservationatv.model.entities;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class Reservation {
+    private Integer roomNumber;
+    private Date checkIn;
+    private Date checkOut;
+
+    private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+    
+    public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+        this.roomNumber = roomNumber;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+    public Integer getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(Integer roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public Date getCheckIn() {
+        return checkIn;
+    }
+
+    public Date getCheckOut() {
+        return checkOut;
+    }
+
+    //Estudar mais sobre esse TimeUnit
+    public long duration() {
+        long diff = checkOut.getTime() - checkIn.getTime();
+        return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+    }
+
+    public void updateDates(Date checkIn, Date checkOut) {
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+    @Override
+    public String toString() {
+        return "Room Number: " + roomNumber 
+        + ", Check-in: " + sdf.format(checkIn)
+        + ", Check-out " + sdf.format(checkOut)
+        + ", "
+        + duration()
+        + "Nights";
+    }
+
+}
